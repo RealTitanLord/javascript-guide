695635ddc6ea1acf2738fe94b2c6f4d22905ae9e
Titan Lord <abhinnapandit5@gmail.com>
2024-02-13 23:29:40 +0530

    Initial commit

---
+++ 01_Basic/02_datatypes.js
@@ -0,0 +1,29 @@
+/*
+
+Datatypes Is kinda Nightmare for some users.
+But Don't worry I Got you Covered
+
+
+
+*/
+
+"use strict"; // This will treat all JS code as a Newer version
+
+//alert()  This is used to make pop-ups for Browser, Will Learn More about it in further
+
+let number = 35 // Represent Number 
+let bigint = 1568415468410.112 // Represent Float / Decimal value
+let string = "I am a String value" // Represent Words
+let Boolean = true // Represent Condition true / false
+let nothing = null // Explicitly signifies no value
+let notdefined = undefined //Variable declared but not assigned
+// last is Symbol this we will learn more in future
+
+console.log(number, bigint, string, Boolean, nothing, notdefined);
+
+console.log(typeof number)
+console.log(typeof bigint)
+console.log(typeof string)
+console.log(typeof Boolean)
+console.log(typeof notdefined)
+console.log(typeof nothing)
